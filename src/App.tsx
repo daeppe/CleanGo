b3fb940e9e3c0c14117ca0f85485cf5a4f0c71da
Andre Flor <andreflor96@hotmail.com>
2021-07-07 14:37:40 -0300

    fix: conflito local

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import GlobalStyle from "./style/global";
-<<<<<<< HEAD
-import Header from "./components/Header";
-=======
->>>>>>> develop
 import Routes from "./routes";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Header>
-        <p>teste</p>
-      </Header>
-=======
->>>>>>> develop
       <Routes />
       <GlobalStyle />
     </>
