c75486983143cc7a48db4e756a7b0de5e8b040a6
Guilherme Lemes de Freitas <guilemes300@gmail.com>
2021-07-08 10:11:04 -0300

    feature: context partner

--- src/providers/Partners/index.tsx
+++ src/providers/Partners/index.tsx
@@ -1,11 +1,8 @@
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useContext,
-  useState,
-} from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../../services/api";
+import { notification } from "antd";
+import { FaTimesCircle, FaTimes, FaCheckCircle } from "react-icons/fa";
 
 interface Partner {
   name: string;
@@ -27,6 +24,8 @@ interface Partner {
 
 interface PartnersContextProps {
   partners: Partner[];
+  newPartner: (partner: Partner) => void;
+  editPartner: (partner: Partner, id: string, token: string) => void;
 }
 
 interface PartnersProviderProps {
@@ -39,11 +38,77 @@ const PartnersContext = createContext<PartnersContextProps>(
 
 export const PartnersProvider = ({ children }: PartnersProviderProps) => {
   const [partners, setPartners] = useState<Partner[]>([]);
+  const history = useHistory();
 
-  const newPartner = (partner: Partner) => {};
+  const newPartner = (partner: Partner) => {
+    api
+      .post("register/", partner)
+      .then((_) => {
+        history.push("/login");
+        notification.open({
+          message: "Sucesso",
+          closeIcon: <FaTimes />,
+          style: {
+            fontFamily: "Roboto",
+            backgroundColor: "var(--gray)",
+            WebkitBorderRadius: 4,
+          },
+          description: "Usuário criado.",
+          icon: <FaCheckCircle style={{ color: "green" }} />,
+        });
+      })
+      .catch((_) =>
+        notification.open({
+          message: "Erro.",
+          closeIcon: <FaTimes />,
+          style: {
+            fontFamily: "Roboto",
+            backgroundColor: "var(--gray)",
+            WebkitBorderRadius: 4,
+          },
+          description: "Erro ao realizar cadastro.",
+          icon: <FaTimesCircle style={{ color: "red" }} />,
+        })
+      );
+  };
+
+  const editPartner = (partner: Partner, id: string, token: string) => {
+    api
+      .patch(`users/${id}`, partner, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((_) => {
+        notification.open({
+          message: "Sucesso",
+          closeIcon: <FaTimes />,
+          style: {
+            fontFamily: "Roboto",
+            backgroundColor: "var(--gray)",
+            WebkitBorderRadius: 4,
+          },
+          description: "Dados atualizados",
+          icon: <FaCheckCircle style={{ color: "green" }} />,
+        });
+      })
+      .catch((_) =>
+        notification.open({
+          message: "Erro.",
+          closeIcon: <FaTimes />,
+          style: {
+            fontFamily: "Roboto",
+            backgroundColor: "var(--gray)",
+            WebkitBorderRadius: 4,
+          },
+          description: "Erro ao atualizar informações.",
+          icon: <FaTimesCircle style={{ color: "red" }} />,
+        })
+      );
+  };
 
   return (
-    <PartnersContext.Provider value={{ partners }}>
+    <PartnersContext.Provider value={{ partners, newPartner, editPartner }}>
       {children}
     </PartnersContext.Provider>
   );
