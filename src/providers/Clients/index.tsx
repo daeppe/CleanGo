9fd0a37da3f1eb25b4f71a0f822d7b4f1bdee722
Kelsiane Lima <kelsianelima74@gmail.com>
2021-07-08 01:10:16 -0300

    feature: editar cliente pronto

--- src/providers/Clients/index.tsx
+++ src/providers/Clients/index.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import { ClientData } from "../../types/clientData";
 import api from "../../services/api";
+import jwt_decode from "jwt-decode";
 
 interface ClientProviderProps {
   children: ReactNode;
@@ -9,13 +10,30 @@ interface ClientProviderProps {
 interface ClientProviderData {
   newClient: (clientData: ClientData) => void;
   clientLogin: (clientData: ClientLogin) => void;
+  editClient: (clientData: ClientLogin) => void;
   token: string;
   setAuth: (value: React.SetStateAction<string>) => void;
+  idClient: string;
+  setIdClient: (value: React.SetStateAction<string>) => void;
 }
 
 interface ClientLogin {
   email: string;
   password: string;
+}
+interface EditClient {
+  partner?: boolean;
+  name?: string;
+  email?: string;
+  cpf?: string;
+  password?: string;
+}
+
+interface DecodeToken {
+  sub: string;
+  iat: number;
+  email: string;
+  exp: number;
 }
 
 const ClientContext = createContext<ClientProviderData>(
@@ -26,7 +44,6 @@ export const ClientProvider = ({ children }: ClientProviderProps) => {
   // registrar cliente
 
   const newClient = (clientData: ClientData) => {
-    console.log(clientData);
     api
       .post("register", clientData)
       .then((response) => {
@@ -39,21 +56,43 @@ export const ClientProvider = ({ children }: ClientProviderProps) => {
   //  login de cliente
   const token = localStorage.getItem("token") || "";
   const [auth, setAuth] = useState<string>(token);
-
+  const [idClient, setIdClient] = useState<string>("");
   const clientLogin = (clientData: ClientLogin) => {
     api
       .post("login", clientData)
       .then((response) => {
         localStorage.setItem("token", response.data.accessToken);
+        const decodedToken: DecodeToken = jwt_decode(response.data.accessToken);
+        localStorage.setItem("idClient", decodedToken.sub);
+        setIdClient(decodedToken.sub);
         setAuth(response.data.accessToken);
         // history.push("/dashboard");
       })
       .catch((err) => console.log(err));
   };
 
+  const editClient = (clientData: EditClient) => {
+    api
+      .patch(`users/${idClient}`, clientData, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <ClientContext.Provider
-      value={{ token: auth, setAuth, newClient, clientLogin }}
+      value={{
+        token: auth,
+        setAuth,
+        newClient,
+        clientLogin,
+        editClient,
+        idClient,
+        setIdClient,
+      }}
     >
       {children}
     </ClientContext.Provider>
@@ -61,6 +100,3 @@ export const ClientProvider = ({ children }: ClientProviderProps) => {
 };
 
 export const useClients = () => useContext(ClientContext);
-
-// setError: Dispatch<SetStateAction<boolean>>,
-// history: History
