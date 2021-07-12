4d66394b1b016ae95e7230e2bd683d61f595910d
Guilherme Lemes de Freitas <guilemes300@gmail.com>
2021-07-12 19:21:15 -0300

    feature: adicionando estilo mobile ao form de solicitação

--- src/components/BackgroundGray/styles.ts
+++ src/components/BackgroundGray/styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.div`
   inset: 0;
   width: 100%;
   max-width: 100vw;
-  height: 100%;
+  height: 100vh;
   background-color: var(--light-gray);
 
   ${(props: ContainerProps) =>
