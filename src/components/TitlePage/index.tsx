ea70b28b2425d066cb8339e96ad276fc8f86ff95
Kelsiane Lima <kelsianelima74@gmail.com>
2021-07-13 17:00:09 -0300

    feature:title page em todas as paginas

--- src/components/TitlePage/index.tsx
+++ src/components/TitlePage/index.tsx
@@ -1,12 +1,10 @@
 import Helmet from "react-helmet";
-// import { useLocation } from "react-router-dom";
-// import { formatedPath } from "../../utils/formatedPath";
+
 interface TitlePageProps {
   title: string;
 }
 const TitlePage = ({ title }: TitlePageProps) => {
-  // const location = useLocation();
-  return <Helmet title={`CleanGo | Página ${title}`} />;
+  return <Helmet title={`CleanGo | ${title}`} />;
 };
 
 export default TitlePage;
