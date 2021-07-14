93ecbe6e2bb0df312e549a6cdb4b5d014aa1c745
Andre Flor <andreflor96@hotmail.com>
2021-07-14 11:49:43 -0300

    feat: secao como funciona

--- src/components/CardWorkWithUs/index.tsx
+++ src/components/CardWorkWithUs/index.tsx
@@ -7,15 +7,18 @@ const CardWorkWithUs = () => {
     <Container>
       <ContainerInner>
         <img src={Register} alt="cadastro" />
-        <h1>Cadastro</h1>
+        <h1 className="cardTitle">Cadastro</h1>
+        <p>Coloque suas informações e receba os melhores serviços.</p>
       </ContainerInner>
       <ContainerInner>
         <img src={Return} alt="cadastro" />
-        <h1>Aguarde o Retorno</h1>
+        <h1 className="cardTitle">Acesso ao Aplicativo</h1>
+        <p>Tenha uma plataforma só sua para gerenciar seus serviços.</p>
       </ContainerInner>
       <ContainerInner>
         <img src={AppAccess} alt="cadastro" />
-        <h1>Acesso ao Aplicativo</h1>
+        <h1 className="cardTitle">Busque um serviço</h1>
+        <p>Veja todos os serviços e selecione apenas os que você quiser.</p>
       </ContainerInner>
     </Container>
   );
--- src/pages/WorkWithUs/styles.ts
+++ src/pages/WorkWithUs/styles.ts
@@ -7,6 +7,8 @@ export const Container = styled.main`
   justify-content: center;
   background-color: var(--dark-green);
   margin-top: -90px;
+  position: relative;
+  /* min-height: 100vh; */
 `;
 export const SectionStyled = styled.section`
   display: flex;
@@ -14,6 +16,7 @@ export const SectionStyled = styled.section`
   background-color: var(--white);
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 export const SectionPartner = styled(SectionStyled)`
   margin: 90px 0;
@@ -24,15 +27,24 @@ export const SectionPartner = styled(SectionStyled)`
 `;
 export const SectionWhowItWorks = styled(SectionStyled)`
   flex-direction: column;
+  padding: 5rem 0;
   h1 {
-    margin: 16px 0 32px;
+    margin: 0 0 3rem;
+  }
+  @media only screen and (min-width: 768px) {
+    padding: 6rem 0;
+    h1 {
+      margin: 0 0 5rem;
+    }
   }
 `;
-export const SectionReasons = styled(SectionWhowItWorks)`
+export const SectionReasons = styled(SectionStyled)`
+  flex-direction: column;
   background-color: var(--dark-green);
   h1 {
     color: var(--white);
   }
+  padding: 5rem 0;
 `;
 export const Title = styled.h1`
   font-size: 36px;
